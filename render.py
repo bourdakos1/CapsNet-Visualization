2a0827a1f5183d1ac40060b4b794ef8d47cdbc5a
Nick Bourdakos <bourdakos1@gmail.com>
2018-02-13 01:17:14 -0500

    update

--- render.py
+++ render.py
@@ -61,9 +61,9 @@ PATH_TO_WEIGHTS_FULLY_CONNECTED3_BIAS = os.path.join(PATH_TO_WEIGHTS, 'fully_con
 NUMBER_OF_ROUNDS = 3
 
 # Load the weights with numpy.
-conv1 = np.load(PATH_TO_WEIGHTS_CONV1)
+conv1_weights = np.load(PATH_TO_WEIGHTS_CONV1)
 conv1_bias = np.load(PATH_TO_WEIGHTS_CONV1_BIAS)
-primary_caps = np.load(PATH_TO_WEIGHTS_PRIMARY_CAPS)
+primary_caps_weights = np.load(PATH_TO_WEIGHTS_PRIMARY_CAPS)
 primary_caps_bias = np.load(PATH_TO_WEIGHTS_PRIMARY_CAPS_BIAS)
 digit_caps = np.load(PATH_TO_WEIGHTS_DIGIT_CAPS)
 fully_connected1 = np.load(PATH_TO_WEIGHTS_FULLY_CONNECTED1)
@@ -73,10 +73,9 @@ fully_connected1_bias = np.load(PATH_TO_WEIGHTS_FULLY_CONNECTED1_BIAS)
 fully_connected2_bias = np.load(PATH_TO_WEIGHTS_FULLY_CONNECTED2_BIAS)
 fully_connected3_bias = np.load(PATH_TO_WEIGHTS_FULLY_CONNECTED3_BIAS)
 
-output = np.empty((20, 20, 256))
-primary_caps_output = np.empty((6, 6, 256))
-
-
+################################################################################
+# Helper Functions
+################################################################################
 def squash(s, axis=-1, epsilon=1e-9):
     squared_norm = np.sum(np.square(s), axis=axis, keepdims=True)
     safe_norm = np.sqrt(squared_norm + epsilon)
@@ -89,50 +88,73 @@ def safe_norm(s, axis=-1, epsilon=1e-9, keepdims=False):
     squared_norm = np.sum(np.square(s), axis=axis, keepdims=keepdims)
     return np.sqrt(squared_norm + epsilon)
 
-# Save original image to the visualization folder.
+
+expit = lambda x: 1.0 / (1 + np.exp(-x))
+def sigmoid_function(signal):
+    # Prevent overflow.
+    signal = np.clip(signal, -500, 500)
+    # Calculate activation signal
+    return expit(signal)
+
+
+def ReLU_function(signal):
+    # Return the activation signal
+    return np.maximum(0, signal)
+
+
+################################################################################
+# Load Input Image
+################################################################################
 img = Image.open(os.path.join(PATH_TO_TEST_IMAGES_DIR, PATH_TO_TEST_IMAGE))
+# Save original image to the visualization folder
 image.imsave(os.path.join(PATH_TO_INPUT_IMAGE, '{}.png'.format(0)), img)
+input_image = np.array(img.getdata(), dtype=np.uint8)
+input_image = np.resize(input_image, (img.size[1], img.size[0], 4))
+input_image = input_image[:, :, 1]
 
-for i in range(conv1.shape[3]):
-    # Get the 9x9x1 filter:
-    extracted_filter = conv1[:, :, :, i]
 
-    # Get rid of the last dimension (hence get 9x9):
+################################################################################
+# Convolution 1
+################################################################################
+conv1_output = np.empty((20, 20, 256))
+for i in range(conv1_weights.shape[3]):
+    # Get the 9x9 kernel
+    extracted_filter = conv1_weights[:, :, :, i]
     extracted_filter = np.squeeze(extracted_filter)
 
+    # Save image of the kernel
     image.imsave(os.path.join(PATH_TO_CONV1_KERNEL, '{}.png'.format(i)), extracted_filter, vmin=-0.6064218, vmax=0.24946211)
 
-    img = Image.open(os.path.join(PATH_TO_TEST_IMAGES_DIR, PATH_TO_TEST_IMAGE))
-    arr2 = np.array(img.getdata(), dtype=np.uint8)
+    # Apply convolution
+    conv1 = signal.correlate2d(input_image, extracted_filter, 'valid')
+    conv1 = conv1 + conv1_bias[i]
 
-    arr2 = np.resize(arr2, (img.size[1], img.size[0], 4))
-    arr2 = arr2[:, :, 1]
+    # ReLU
+    conv1 = np.maximum(0, conv1)
+    conv1_output[:, :, i] = conv1
 
-    conv = signal.correlate2d(arr2, extracted_filter, 'valid')
-
-    conv = conv + conv1_bias[i]
-
-    conv = np.maximum(0, conv)
-    output[:, :, i] = conv
-
-    image.imsave(os.path.join(PATH_TO_RELU, '{}.png'.format(i)), conv, cmap=CMAP, vmin=0, vmax=255)
+    # Save image of the the convolution and ReLU
+    image.imsave(os.path.join(PATH_TO_RELU, '{}.png'.format(i)), conv1, cmap=CMAP, vmin=0, vmax=255)
 
 
-for i in range(primary_caps.shape[3]):
-    # 9,9,256,256
-    extracted_filter = primary_caps[:, :, :, i]
+################################################################################
+# PrimaryCaps
+################################################################################
+primary_caps_output = np.empty((6, 6, 256))
+for i in range(primary_caps_weights.shape[3]):
+    # Get the 9x9x256 kernel
+    extracted_filter = primary_caps_weights[:, :, :, i]
 
-    conv2 = signal.correlate(output, extracted_filter, 'valid')
-
-    # Outputs 12x12, but we need 6x6 so we need to somehow drop every other item
-    conv2 = conv2[::2, ::2, :]
-
-    conv2 = np.squeeze(conv2)
-
+    # Apply convolution
+    conv2 = signal.correlate(conv1_output, extracted_filter, 'valid')
     conv2 = conv2 + primary_caps_bias[i]
 
-    conv2 = np.maximum(0, conv2)
+    # Outputs 12x12, but we need 6x6 so we drop every other item
+    conv2 = conv2[::2, ::2, :]
+    conv2 = np.squeeze(conv2)
 
+    # ReLU
+    conv2 = np.maximum(0, conv2)
     primary_caps_output[:, :, i] = conv2
 
 
@@ -145,17 +167,34 @@ primary_caps_output = np.reshape(primary_caps_output, (-1, 8, 1))
 # Squash
 squashed_caps_output = squash(primary_caps_output)
 
-
-# This is completely useless, but it makes it easier to visualize
+# Render the Capsule Layers after squashing
 normed_squashed_caps_output = np.reshape(safe_norm(squashed_caps_output, axis=-2), (6, 6, 32))
 for i in range(normed_squashed_caps_output.shape[2]):
     image.imsave(os.path.join(PATH_TO_PRIMARY_CAPS, '{}.png'.format(i)), normed_squashed_caps_output[:, :, i], cmap=CMAP, vmin=0, vmax=1)
 
 
-# Add in a blank dimension: 1152, 1, 8, 1
+################################################################################
+# DigitCaps and Routing
+################################################################################
+
+# Add in a blank dimension: (1, 1152, 1, 8, 1)
 squashed_caps_output = np.reshape(squashed_caps_output, (-1, 1, squashed_caps_output.shape[-2], 1))
-# Tile the capsules in the inserted dimension: 1152, 10, 8, 1
+# Tile the capsules in the inserted dimension: (1, 1152, 10, 8, 1)
 caps_output_tiled = np.tile(squashed_caps_output, [1, 1, 10, 1, 1])
+
+# Transpose DigitCaps (1, 1152, 10, 8, 16) -> (1, 1152, 10, 16, 8)
+# matmul caps_output_tiled (1152, 10, 8, 1) by digit_caps (1, 1152, 10, 16, 8)
+# It's doing a matrix multiplication on the last 2 dimensions:
+#                 │ 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0
+#                 │ 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
+# ────────────────┼────────────────────────────────
+# 1 2 3 4 5 6 7 8 │ 1 2 3 4 5 6 7 8 0 0 0 0 0 0 0 0
 
 caps2_predicted = np.matmul(np.transpose(digit_caps, (0, 1, 2, 4, 3)), caps_output_tiled)
 
@@ -172,7 +211,7 @@ for x in range(0, NUMBER_OF_ROUNDS):
     caps2_output = squash(weighted_sum, axis=-2)
 
     # We don't need to recalcute raw weights on the last iteration
-    if x != NUMBER_OF_ROUNDS - 1:
+    if x < NUMBER_OF_ROUNDS:
         # Add dot product to the weights
         caps2_output_tiled = np.tile(caps2_output, [1, 1152, 1, 1, 1])
         agreement = np.matmul(np.transpose(caps2_predicted, (0, 1, 2, 4, 3)), caps2_output_tiled)
@@ -185,27 +224,23 @@ y_proba = safe_norm(caps2_output, axis=-2)
 digit_caps_image = y_proba.reshape(10, 1)
 image.imsave(os.path.join(PATH_TO_DIGIT_CAPS, '{}.png'.format(0)), digit_caps_image, cmap=CMAP, vmin=0, vmax=1)
 
+
+################################################################################
+# Prediction
+################################################################################
+
 y_proba_argmax = np.argmax(y_proba, axis=2)
 y_pred = np.squeeze(y_proba_argmax, axis=[1,2])
 
 print('Prediction: {}'.format(y_pred))
 
+
+################################################################################
+# Reconstruction
+################################################################################
+
 caps2_output = np.squeeze(caps2_output)
 reconstruction_input = caps2_output[y_pred]
-
-expit = lambda x: 1.0 / (1 + np.exp(-x))
-def sigmoid_function(signal):
-    # Prevent overflow.
-    signal = np.clip(signal, -500, 500)
-
-    # Calculate activation signal
-    return expit(signal)
-
-
-def ReLU_function(signal):
-    # Return the activation signal
-    return np.maximum(0, signal)
-
 
 output = reconstruction_input
 
